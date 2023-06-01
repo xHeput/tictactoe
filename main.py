fa3385b962892174cf949733f01162ab0e836266
xHeput <maciek.niemiec9@wp.pl>
2023-06-01 23:50:42 +0200

    Game not finished - when neither side has three in a row but the grid still has empty cells. Draw - when no side has a three in a row and the grid has no empty cells. X wins - when the grid has three X's in a row (including diagonals). O wins - when the grid has three O's in a row (including diagonals). Impossible - when the grid has three X's in a row as well as three O's in a row, or there are a lot more X's than O's or vice versa (the difference should be 1 or 0; if the difference is 2 or more, then the game state is impossible).

--- main.py
+++ main.py
@@ -7,37 +7,66 @@ print("|", user_input[3], user_input[4], user_input[5], "|")
 print("|", user_input[6], user_input[7], user_input[8], "|")
 print("---------")
 
-
+x_win = 0
+o_win = 0
+not_win_num = 0
 # checks for win in diagonal axes
 for i in ["X", "O"]:
     if user_input[0] == i and user_input[4] == i and user_input[8] == i:
         print(i, "wins")
-        exit()
     elif user_input[2] == i and user_input[4] == i and user_input[6] == i:
         print(i, "wins")
-        exit()
+    else:
+        not_win_num += 1
 
-# checks for win in horizontal line
+
+# checks for win in horizontal = poziomy line
 for i in [0, 3, 6]:
     if user_input[i] == "X" and user_input[i + 1] == "X" and user_input[i + 2] == "X":
-        print("X wins")
-        exit()
+        x_win += 1
     elif user_input[i] == "O" and user_input[i + 1] == "O" and user_input[i + 2] == "O":
-        print("O wins")
-        exit()
+        o_win += 1
+    else:
+        not_win_num += 1
 
-# checks for win in vertical line
+
+# checks for win in vertical = pionowy line
 for j in range(3):
     if user_input[j] == "X" and user_input[j + 3] == "X" and user_input[j + 6] == "X":
-        print("X wins")
-        exit()
+        x_win += 1
     elif user_input[j] == "O" and user_input[j + 3] == "O" and user_input[j + 6] == "O":
-        print("O wins")
-        exit()
+        o_win += 1
+    else:
+        not_win_num += 1
 
-for i in range(len(user_input)):
-    if user_input[i] == "_":
-        print("Game not finished")
-        exit()
+x_number = 0
+o_number = 0
+# checking if there are a lot more X's than O's or vice versa
+for i in range(9):
+    if user_input[i] == "X":
+        x_number += 1
+    elif user_input[i] == "O":
+        o_number += 1
+if (x_number - o_number) >= 2 or (o_number - x_number) >= 2:
+    print("Impossible")
+    exit()
 
-print("Draw")
+# prints who won
+if o_win > 0 and x_win > 0:
+    print("Impossible")
+    exit()
+elif x_win == 1:
+    print("X wins")
+elif o_win == 1:
+    print("O wins")
+
+# checking for missing poles and draw
+number_of_dashes = 0
+if not_win_num >= 8:
+    for i in range(len(user_input)):
+        if user_input[i] == "_":
+            print("Game not finished")
+            number_of_dashes += 1
+            exit()
+    if number_of_dashes == 0:
+        print("Draw")
